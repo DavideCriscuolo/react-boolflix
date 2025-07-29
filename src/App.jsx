cfb22bdce7f4ca14fb06a7c5e836afc37c3336a6
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-29 11:45:11 +0200

    aggiunta libreria per convertire la chiave original_language in una bandierina  e renderizata

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ film trovato:
 1.  Titolo 
 2.  Titolo Originale 
 3.  Lingua 
-4.  Voto  */
+4.  Voto  
 
-import axios from "axios";
+
+Milestone 2: 
+Trasformiamo la stringa statica della lingua in una vera e propria bandiera della 
+nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della 
+nazione ritornata dall’API (le flag non ci sono in FontAwesome). 
+ 
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca 
+dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando 
+attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di 
+risposta diversi, simili ma non sempre identici) 
+Qui un esempio di chiamata per le serie tv: 
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
+crubs 
+ 
+
+
+
+*/
+import Flag from "react-world-flags"; // per le bandiere
 import { use, useEffect, useState } from "react";
 
 function App() {
@@ -45,8 +63,11 @@ function App() {
         return (
           <ul key={film.id}>
             <li>Titolo: {film.title} </li>
-            <li> Titolo Originale:{film.original_title}</li>
-            <li>Lingua: {film.original_language}</li>
+            <li> Titolo Originale:{film.original_title}</li>{" "}
+            <Flag
+              code={film.original_language.toUpperCase()}
+              style={{ width: 30, height: 20 }}
+            ></Flag>
             <li>Voto: {film.vote_average}</li>
           </ul>
         );
